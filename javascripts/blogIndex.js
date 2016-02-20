3f171d21ea1c2bb79dcbe697c1704c806a64cc33
HuangFuGui <1151650717@qq.com>
2016-02-20 11:39:25 +0800

    improve blogIndex.html

---
+++ javascripts/blogIndex.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+
+
+$(function ()
+ {
+	 
+	/**************************
+	监听屏幕并相应改变导航栏属性
+	**************************/
+	  $(window).scroll(function()
+	  {
+		  if ($(window).scrollTop()>450)
+		  {
+			  $("#blogIndex_header a").css("color","#000000");
+			  $("#blogIndex_header").css("background-color","rgba(50, 50, 50, 0.1)");
+		  }
+		  else 
+	  	  {
+			  $("#blogIndex_header a").css("color","#FFFFFF");
+			  $("#blogIndex_header").css("background-color","rgba(150, 150, 150, 0)");
+		  }
+				  	 
+	  });
+	  //当点击跳转链接后，回到页面顶部位置
+	  /*	  $("#back-to-top").click(function()
+	  {
+		  $('body,html').animate({scrollTop:0},1000);
+		  return false;
+	  });*/
+ });
